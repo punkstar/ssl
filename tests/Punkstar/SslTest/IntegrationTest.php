2fe0a72aaddb433b07e93cd77f0444ad0ef9bdc7
Nick Jones <nick@nicksays.co.uk>
2016-07-24 16:54:42 +0100

    Add tests for getting raw certificate data

--- tests/Punkstar/SslTest/IntegrationTest.php
+++ tests/Punkstar/SslTest/IntegrationTest.php
@@ -17,8 +17,23 @@ class IntegrationTest extends TestCase
         $cert = $reader->readFromFile($certFileName);
 
         $this->assertInternalType('string', $cert->certName());
+        $this->assertInternalType('string', $cert->toString());
         $this->assertInstanceOf('DateTime', $cert->validTo());
         $this->assertInstanceOf('DateTime', $cert->validFrom());
+    }
+
+    /**
+     * @test
+     * @dataProvider exampleCertsDataProvider
+     */
+    public function testRawCertificate($certFileName)
+    {
+        $reader = new Reader();
+        $cert = $reader->readFromFile($certFileName);
+
+        $rawCertFromFile = file_get_contents($certFileName);
+
+        $this->assertEquals($rawCertFromFile, $cert->toString());
     }
 
     public function exampleCertsDataProvider()
