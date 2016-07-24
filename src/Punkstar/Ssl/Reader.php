ffb86b2f7b4beed40d390b60be5b8f12cf729144
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-07-24 17:11:27 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Punkstar/Ssl/Reader.php
+++ src/Punkstar/Ssl/Reader.php
@@ -14,7 +14,7 @@ class Reader
         $urlHost = parse_url($url, PHP_URL_HOST);
 
         $streamContext = stream_context_create(array("ssl" => array("capture_peer_cert" => TRUE)));
-        $stream = stream_socket_client("ssl://".$urlHost.":443", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $streamContext);
+        $stream = stream_socket_client("ssl://" . $urlHost . ":443", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $streamContext);
         $streamParams = stream_context_get_params($stream);
 
         $certResource = $streamParams['options']['ssl']['peer_certificate'];
