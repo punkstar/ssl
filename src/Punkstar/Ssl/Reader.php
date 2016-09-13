1d2318224b98323f2bdcb2283182316f82b26916
Nick <nick@nicksays.co.uk>
2016-09-13 07:51:30 +0100

    Don't verify the certificate when fetching (#2)
    This library is about reading certificates, not about verifying the
    certificates themselves.

--- src/Punkstar/Ssl/Reader.php
+++ src/Punkstar/Ssl/Reader.php
@@ -13,7 +13,14 @@ class Reader
     {
         $urlHost = parse_url($url, PHP_URL_HOST);
 
-        $streamContext = stream_context_create(array("ssl" => array("capture_peer_cert" => TRUE)));
+        $streamContext = stream_context_create(array(
+            "ssl" => array(
+                "capture_peer_cert" => TRUE,
+                "verify_peer" => FALSE,
+                "verify_peer_name" => FALSE
+            )
+        ));
+
         $stream = stream_socket_client("ssl://" . $urlHost . ":443", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $streamContext);
         $streamParams = stream_context_get_params($stream);
 
